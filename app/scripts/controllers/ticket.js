cddc275ca2175bc14ff8ceedc2043cb3866be629
egmfilho <edumig12@hotmail.com>
2016-06-22 17:31:47 -0300

    tela de tickets

--- app/scripts/controllers/ticket.js
+++ app/scripts/controllers/ticket.js
@@ -2,103 +2,62 @@
  * Created by egmfilho on 21/06/16.
  */
 'use strict';
-
 angular.module('belissimaApp')
   .controller('TicketCtrl', ['$scope', function ($scope) {
 
-    $scope.hoje = function() {
-      $scope.dt = new Date();
-    };
-    $scope.hoje();
-
-    $scope.limpar = function() {
-      $scope.dt = null;
-    };
-
-    $scope.inlineOptions = {
-      customClass: getDayClass,
-      minDate: new Date(),
-      showWeeks: true
-    };
-
-    $scope.dateOptions = {
-      dateDisabled: disabled,
-      formatYear: 'yy',
-      maxDate: new Date(2020, 5, 22),
-      minDate: new Date(),
-      startingDay: 0,
-      showWeeks: false,
-    };
-
-    // Disable weekend selection
-    function disabled(data) {
-      var date = data.date,
-        mode = data.mode;
-      //return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
-      return false;
-    }
-
-    $scope.toggleMin = function() {
-      $scope.inlineOptions.minDate = $scope.inlineOptions.minDate ? null : new Date();
-      $scope.dateOptions.minDate = $scope.inlineOptions.minDate;
-    };
-
-    $scope.toggleMin();
-
-    $scope.open1 = function() {
-      $scope.popup1.opened = true;
-    };
-
-    $scope.open2 = function() {
-      $scope.popup2.opened = true;
-    };
-
-    $scope.setDate = function(year, month, day) {
-      $scope.dt = new Date(year, month, day);
-    };
+    $scope.inicial = new Date();
+    $scope.final = new Date();
 
     $scope.format = 'dd/MM/yy';
     $scope.altInputFormats = ['d!/M!/yy'];
 
-    $scope.popup1 = {
-      opened: false
+    $scope.dateOptionsInicial = {
+      dateDisabled: disabled,
+      formatYear: 'yy',
+      maxDate: new Date(), // Hoje
+      //minDate: new Date(2016, 5, 1),
+      startingDay: 0,
+      showWeeks: false,
     };
 
-    $scope.popup2 = {
-      opened: false
+    $scope.dateOptionsFinal = {
+      dateDisabled: disabled,
+      formatYear: 'yy',
+      maxDate: new Date(), // Hoje
+      minDate: $scope.inicial,
+      startingDay: 0,
+      showWeeks: false
     };
 
-    var tomorrow = new Date();
-    tomorrow.setDate(tomorrow.getDate() + 1);
-    var afterTomorrow = new Date();
-    afterTomorrow.setDate(tomorrow.getDate() + 1);
-    $scope.events = [
-      {
-        date: tomorrow,
-        status: 'full'
-      },
-      {
-        date: afterTomorrow,
-        status: 'partially'
-      }
-    ];
-
-    function getDayClass(data) {
+    // Desabilita dias
+    function disabled(data) {
       var date = data.date,
         mode = data.mode;
-      if (mode === 'day') {
-        var dayToCheck = new Date(date).setHours(0,0,0,0);
 
-        for (var i = 0; i < $scope.events.length; i++) {
-          var currentDay = new Date($scope.events[i].date).setHours(0,0,0,0);
+      // Desabilita finais de semana
+      //return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
 
-          if (dayToCheck === currentDay) {
-            return $scope.events[i].status;
-          }
-        }
-      }
-
-      return '';
+      return false;
     }
 
-  }]);
+    $scope.abrirInicial = function() {
+      $scope.popupInicial.opened = true;
+    };
+
+    $scope.popupInicial = {
+      opened: false
+    };
+
+    $scope.abrirFinal = function() {
+      $scope.popupFinal.opened = true;
+    };
+
+    $scope.popupFinal = {
+      opened: false
+    };
+
+    $scope.trocaMinDataFinal = function() {
+      $scope.dateOptionsFinal.minDate = $scope.inicial;
+    };
+
+}]);
